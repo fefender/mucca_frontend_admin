9d540e9d28b35f02732c43e24e669f1b0b95c941
fefender <fefender@gmail.com>
2019-04-30 16:00:48 +0200

    home++

--- src/Components/Sections/AppConfig.jsx
+++ src/Components/Sections/AppConfig.jsx
@@ -8,7 +8,10 @@ import {
   Button,
   Table,
   Label,
-  Grid
+  Accordion,
+  Grid,
+  Segment,
+  Transition
 } from "semantic-ui-react";
 
 const api = new Api();
@@ -25,10 +28,41 @@ class AppConfig extends Component {
     portList: "",
     coreEventConsumer: "",
     mpkgEventConsumer: "",
-    loaded: false
+    loaded: false,
+    active0: true,
+    active1: false,
+    active2: false,
+    active3: false,
+    active4: false,
+    active5: false,
+    duration: 200
   };
 
+  //Segment Visibility Handlers
+  handleClick0 = e => {
+    this.setState({
+      active0: !this.state.active0
+    });
+  };
+  handleClick1 = e => {
+    this.setState({ active1: !this.state.active1 });
+  };
+  handleClick2 = e => {
+    this.setState({ active2: !this.state.active2 });
+  };
+  handleClick3 = e => {
+    this.setState({ active3: !this.state.active3 });
+  };
+  handleClick4 = e => {
+    this.setState({ active4: !this.state.active4 });
+  };
+  handleClick5 = e => {
+    this.setState({ active5: !this.state.active5 });
+  };
+
+  //Data getters
   getConfigData = res => {
+    console.log(res.data);
     if (res && res.status === 200) {
       this.setState({
         appInfo: res.data.data.appinfo,
@@ -42,16 +76,16 @@ class AppConfig extends Component {
       });
     }
   };
-
   getPortList = res => {
     if (res && res.status === 200) {
       this.setState({ portList: res.data.data[this.state.environment] });
     }
   };
 
+  //Data Setters
   setAppInfo() {
     return (
-      <Table color="violet">
+      <Table>
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>App Name</Table.HeaderCell>
@@ -70,132 +104,6 @@ class AppConfig extends Component {
       </Table>
     );
   }
-  setCore() {
-    const content = [];
-    for (let x in this.state.core) {
-      let rowTitle = (
-        <Table.Row>
-          <Table.Cell>{this.state.core[x].name}</Table.Cell>
-          <Table.Cell>
-            {this.state.portList[this.state.core[x].name]}
-          </Table.Cell>
-          <Table.Cell>
-            <Icon name="checkmark" />
-          </Table.Cell>
-        </Table.Row>
-      );
-      content.push(rowTitle);
-    }
-    return (
-      <Table color="teal">
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Service Name</Table.HeaderCell>
-            <Table.HeaderCell>Port</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        {content.map((value, ind) => (
-          <Table.Body key={ind}>{value}</Table.Body>
-        ))}
-      </Table>
-    );
-  }
-  setMpkg() {
-    const content = [];
-    for (let x in this.state.mpkg) {
-      let rowTitle = (
-        <Table.Row>
-          <Table.Cell>{this.state.mpkg[x].name}</Table.Cell>
-          <Table.Cell>
-            {this.state.portList[this.state.mpkg[x].name]}
-          </Table.Cell>
-          <Table.Cell>
-            <Icon name="checkmark" />
-          </Table.Cell>
-        </Table.Row>
-      );
-      content.push(rowTitle);
-    }
-    return (
-      <Table color="purple">
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Service Name</Table.HeaderCell>
-            <Table.HeaderCell>Port</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        {content.map((value, ind) => (
-          <Table.Body key={ind}>{value}</Table.Body>
-        ))}
-      </Table>
-    );
-  }
-
-  setCoreEventConsumer = () => {
-    const content = [];
-    for (let x in this.state.coreEventConsumer) {
-      let rowTitle = (
-        <Table.Row>
-          <Table.Cell>{this.state.coreEventConsumer[x].name}</Table.Cell>
-          <Table.Cell>
-            {this.state.portList[this.state.coreEventConsumer[x].name]}
-          </Table.Cell>
-          <Table.Cell>
-            <Icon name="checkmark" />
-          </Table.Cell>
-        </Table.Row>
-      );
-      content.push(rowTitle);
-    }
-    return (
-      <Table color="orange">
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Service Name</Table.HeaderCell>
-            <Table.HeaderCell>Port</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        {content.map((value, ind) => (
-          <Table.Body key={ind}>{value}</Table.Body>
-        ))}
-      </Table>
-    );
-  };
-
-  setMpkgEventConsumer = () => {
-    const content = [];
-    for (let x in this.state.mpkgEventConsumer) {
-      let rowTitle = (
-        <Table.Row>
-          <Table.Cell>{this.state.mpkgEventConsumer[x].name}</Table.Cell>
-          <Table.Cell>
-            {this.state.portList[this.state.mpkgEventConsumer[x].name]}
-          </Table.Cell>
-          <Table.Cell>
-            <Icon name="checkmark" />
-          </Table.Cell>
-        </Table.Row>
-      );
-      content.push(rowTitle);
-    }
-    return (
-      <Table color="olive">
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Service Name</Table.HeaderCell>
-            <Table.HeaderCell>Port</Table.HeaderCell>
-            <Table.HeaderCell>Status</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        {content.map((value, ind) => (
-          <Table.Body key={ind}>{value}</Table.Body>
-        ))}
-      </Table>
-    );
-  };
 
   setStorage() {}
 
@@ -214,7 +122,7 @@ class AppConfig extends Component {
       content.push(rowTitle);
     }
     return (
-      <Table color="black">
+      <Table>
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>Service Name</Table.HeaderCell>
@@ -232,30 +140,130 @@ class AppConfig extends Component {
   render() {
     return (
       <div className="appConfig">
-        <Header as="h3" color="violet">
-          Application Info{" "}
-        </Header>
-        {this.state.loaded && this.setAppInfo()}
-        <Header as="h3" color="teal">
-          Core Services{" "}
-        </Header>
-        {/* {this.state.loaded && this.genericSetter(this.state.core)} */}
-        {this.state.loaded && this.setCore()}
-        <Header as="h3" color="orange">
-          Core Consumers{" "}
-        </Header>
-        {/* {this.state.loaded && this.genericSetter(this.state.coreEventConsumer)} */}
-        {this.state.loaded && this.setCoreEventConsumer()}
-        <Header as="h3" color="purple">
-          Custom Services{" "}
-        </Header>
-        {/* {this.state.loaded && this.genericSetter(this.state.mpkg)} */}
-        {this.state.loaded && this.setMpkg()}
-        <Header as="h3" color="olive">
-          Custom Consumers{" "}
-        </Header>
-        {/* {this.state.loaded && this.genericSetter(this.state.mpkgEventConsumer)} */}
-        {this.state.loaded && this.setMpkgEventConsumer()}
+        <Accordion exclusive={false}>
+          <Accordion.Title onClick={this.handleClick0}>
+            <Header as="h3" color="blue">
+              Application Info
+              <Icon
+                name="angle down"
+                flipped={this.state.active0 ? "vertically" : "horizontally"}
+              />
+            </Header>
+          </Accordion.Title>
+          <Transition
+            animation="slide down"
+            duration={this.state.duration}
+            visible={this.state.active0}
+          >
+            <Accordion.Content active={this.state.active0}>
+              <Segment color="blue">
+                {this.state.loaded && this.setAppInfo()}
+              </Segment>
+            </Accordion.Content>
+          </Transition>
+          <Accordion.Title onClick={this.handleClick1}>
+            <Header as="h3" color="violet">
+              Core Services
+              <Icon
+                name="angle down"
+                flipped={this.state.active1 ? "vertically" : "horizontally"}
+              />
+            </Header>
+          </Accordion.Title>
+          <Transition
+            animation="slide down"
+            duration={this.state.duration}
+            visible={this.state.active1}
+          >
+            <Accordion.Content active={this.state.active1}>
+              <Segment color="violet">
+                {this.state.loaded && this.genericSetter(this.state.core)}
+              </Segment>
+            </Accordion.Content>
+          </Transition>
+          <Accordion.Title onClick={this.handleClick2}>
+            <Header as="h3" color="purple">
+              Core Consumers
+              <Icon
+                name="angle down"
+                flipped={this.state.active2 ? "vertically" : "horizontally"}
+              />
+            </Header>
+          </Accordion.Title>
+          <Transition
+            animation="slide down"
+            duration={this.state.duration}
+            visible={this.state.active2}
+          >
+            <Accordion.Content active={this.state.active2}>
+              <Segment color="purple">
+                {this.state.loaded &&
+                  this.genericSetter(this.state.coreEventConsumer)}
+              </Segment>
+            </Accordion.Content>
+          </Transition>
+          <Accordion.Title onClick={this.handleClick3}>
+            <Header as="h3" color="red">
+              Custom Services
+              <Icon
+                name="angle down"
+                flipped={this.state.active3 ? "vertically" : "horizontally"}
+              />
+            </Header>
+          </Accordion.Title>
+          <Transition
+            animation="slide down"
+            duration={this.state.duration}
+            visible={this.state.active3}
+          >
+            <Accordion.Content active={this.state.active3}>
+              <Segment color="red">
+                {this.state.loaded && this.genericSetter(this.state.mpkg)}
+              </Segment>
+            </Accordion.Content>
+          </Transition>
+          <Accordion.Title onClick={this.handleClick4}>
+            <Header as="h3" color="orange">
+              Custom Consumers
+              <Icon
+                name="angle down"
+                flipped={this.state.active4 ? "vertically" : "horizontally"}
+              />
+            </Header>
+          </Accordion.Title>
+          <Transition
+            animation="slide down"
+            duration={this.state.duration}
+            visible={this.state.active4}
+          >
+            <Accordion.Content active={this.state.active4}>
+              <Segment color="orange">
+                {this.state.loaded &&
+                  this.genericSetter(this.state.mpkgEventConsumer)}
+              </Segment>
+            </Accordion.Content>
+          </Transition>
+          <Accordion.Title onClick={this.handleClick5}>
+            <Header as="h3" color="teal">
+              Storage
+              <Icon
+                name="angle down"
+                flipped={this.state.active5 ? "vertically" : "horizontally"}
+              />
+            </Header>
+          </Accordion.Title>
+          <Transition
+            animation="slide down"
+            duration={this.state.duration}
+            visible={this.state.active5}
+          >
+            <Accordion.Content active={this.state.active5}>
+              <Segment color="teal">
+                {this.state.loaded && this.genericSetter(this.state.storage)}
+              </Segment>
+            </Accordion.Content>
+          </Transition>
+        </Accordion>
       </div>
     );
   }
